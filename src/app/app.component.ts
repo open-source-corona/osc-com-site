dcfe6d83730ef2a6a90e88138355e3c99ab6bf3c
MBR4 <3e2a2cd980462a66f6947374c5b72d0a9215071a>
2020-03-21 17:19:54 -0400

    Setup and configure basic pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'osc-com-site';
+  title = 'Open Source Corona Project';
+  links = [
+    {
+      label: 'Home', routerLink: ['']
+    },
+    {
+      label: 'Countries', routerLink: ['/countries']
+    },
+    {
+      label: 'Information', routerLink: ['/information']
+    },
+    {
+      label: 'APIs', routerLink: ['/apis']
+    },
+    {
+      label: 'Developers', routerLink: ['/developers']
+    },
+    {
+      label: 'About', routerLink: ['/about']
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MenubarModule, MessagesModule} from 'primeng';
+import {Routes, RouterModule} from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutComponent } from './about/about.component';
+import { DevelopersComponent } from './developers/developers.component';
+import { ApisComponent } from './apis/apis.component';
+import {CountriesComponent} from './countries/countries.component';
+import { InformationComponent } from './information/information.component';
+
+
+const appRoutes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'countries', component: CountriesComponent},
+  {path: 'information', component: InformationComponent},
+  {path: 'developers', component: DevelopersComponent},
+  {path: 'apis', component: ApisComponent},
+  {path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutComponent,
+    DevelopersComponent,
+    ApisComponent,
+    InformationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MenubarModule,
+    MessagesModule,
+    MessagesModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // <-- debugging purposes only
+    ),
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
